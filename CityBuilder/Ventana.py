daa92c1a4f1e81d5a13f79c9cded30f88c03b898
JavaDevOne <javadevone@gmail.com>
2021-01-30 12:00:33 +0100

    Episodio 1
    Creamos la estructura básica: añadimos la ventana, el canvas, el bucle principal a aprox. 60 FPS y hacemos que se pueda cerrar el juego pulsando "q"

---
+++ CityBuilder/Ventana.py
@@ -0,0 +1,46 @@
+from tkinter import Tk, Canvas
+import time
+import sys
+
+class Ventana:
+    def __init__(self, ancho, alto):
+        self.ancho = ancho
+        self.alto = alto
+        self.ventana = Tk()
+        self.ventana.title("Dungeon manager")
+        self.lienzo = Canvas(self.ventana, bg="white", width=self.ancho, height=self.alto)
+        self.lienzo.pack()
+
+        self.msPorActualizacion = 16
+
+        self.aps = 0
+        self.referenciaInicial = time.perf_counter_ns()
+
+        #maquina de estados
+        
+        self.ventana.after(0, self.iterar)
+
+        self.ventana.bind("<Key>", self.tecla)
+
+        self.ventana.mainloop()
+
+    def tecla(self, event):
+        if event.char == "q":
+            print("saliendo del juego")
+            sys.exit(0)
+
+    def iterar(self):
+        referenciaActual = time.perf_counter_ns()
+        nsAcumulados = referenciaActual - self.referenciaInicial
+
+        #actualizamos máquina de estados
+
+        if nsAcumulados / 1000000 >= 1000:
+            print("APS: " + str(self.aps))
+            self.aps = 0
+            self.referenciaInicial = time.perf_counter_ns()
+
+        self.ventana.after(self.msPorActualizacion, self.iterar)
+        self.aps += 1
+
+v = Ventana(800, 600)
