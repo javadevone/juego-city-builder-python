9963fde960dc30c45dce1ff1d4660253305f077c
JavaDevOne <javadevone@gmail.com>
2021-02-15 09:12:07 +0100

    Episodio 03
    Se han añadido la detección de clicks y algunos objetos auxiliares

---
+++ CityBuilder/elementosMapa/Cuadro.py
@@ -0,0 +1,9 @@
+class Cuadro:
+    def __init__(self, x, y, lado, relleno, borde, bordeResaltado, referenciaLienzo):
+        self.x = x
+        self.y = y
+        self.lado =  lado
+        self.relleno =  relleno
+        self.borde =  borde
+        self.bordeResaltado = bordeResaltado 
+        self.referenciaLienzo = referenciaLienzo
---
+++ CityBuilder/elementosMapa/Edificio.py
@@ -0,0 +1,9 @@
+class Edificio:
+    def __init__(self, nombre, categoria, precioOro, precioPiedra, precioMadera, color, referenciaLienzo):
+        self.nombre = nombre 
+        self.categoria = categoria 
+        self.precioOro = precioOro 
+        self.precioPiedra = precioPiedra
+        self.precioMadera = precioMadera
+        self.color = color
+        self.referenciaLienzo = referenciaLienzo
